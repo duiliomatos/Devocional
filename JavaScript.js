2f97763fef3928d1f78c5a605dcb4c6508a9c92e
duiliomatos <duiliomtt@gmail.com>
2025-02-06 10:04:59 -0300

    Comitando

--- JavaScript.js
+++ JavaScript.js
@@ -1,5 +1,4 @@
 //Definição de variável
-
 var nome = "João";   // Forma antiga (evitar)
 let idade = 25;       // Forma correta. Pode ser alterado depois
 const PI = 3.1415;    // Constante, não pode ser alterada
@@ -27,7 +26,6 @@ console.log("true || false:", x || y);
 console.log("!true:", !x);
 
 //Estrutura de Controle
-
 if (condição) {
     //Executa...
 } else if (outra condição) {
